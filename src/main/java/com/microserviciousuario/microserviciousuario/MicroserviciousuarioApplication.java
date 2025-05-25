b69e8da09181334031541e9ecec432422e2a5fc4
Vicente <vishoxcl@gmail.com>
2025-05-24 20:21:53 -0400

    first commit

---
+++ src/main/java/com/microserviciousuario/microserviciousuario/MicroserviciousuarioApplication.java
@@ -0,0 +1,13 @@
+package com.microserviciousuario.microserviciousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciousuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciousuarioApplication.class, args);
+	}
+
+}
